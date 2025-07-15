bcc763ec025aac1c4a97f85f93e4fb6463bf52dd
Divyansh Sharma <divyanshsayz@gmail.com>
2025-07-15 14:14:05 +0530

    Typo in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import { useState } from 'react';
-import RefrshHandler from './RefrshHandler';
+import RefreshHandler from './RefrshHandler';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
